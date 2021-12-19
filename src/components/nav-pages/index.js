344327dc98bf3ceb389408d5d92627264dcc8770
rooreeroo <rureeru@gmail.com>
2021-12-19 20:05:09 +0300

    fix v2

--- src/components/nav-pages/index.js
+++ src/components/nav-pages/index.js
@@ -2,30 +2,16 @@ import React from 'react';
 import './styles.css';
 
 
+function Pages({changePage, pages, page, limit}) {     // changePage - функция | pages - количество товаров (общее) | limit - количество на странице | page - текущая страница
+    let pagesArray = Array.apply(null, {length: Math.ceil(pages/limit)}).map(Number.call, Number);
 
-
-
-function Pages({store, pages}) {
-    let pagesArray = Array.apply(null, {length: pages-1}).map(Number.call, Number);
-    let handleClick = e => {
-        let foo = document.querySelectorAll("a");
-
-        for (let i = 0; i < foo.length; i++) {
-            foo[i].classList.remove("active");
-        }
-
-        e.currentTarget.classList.add("active");
-    };
 
     return (
         <div className="nav_pages">
             <ul>
-                <li onClick={() => store.load(0)}>
-                    <a className={"active"}  onClick={handleClick}>{1}</a>
-                </li>
                 {pagesArray.map(item =>
-                    <li key={item} onClick={() => store.load(item+1)}>
-                        <a  onClick={handleClick}>{item+2}</a>
+                    <li key={item} >
+                        <a className={item === page ? "active" : ''} onClick={() => changePage(item, limit)} >{item+1}</a>
                     </li>
                 )}
             </ul>
